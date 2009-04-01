cf693b7f7ed3238a1f082075d9c059292b4099d3
sche <sche>
2009-04-01 14:26:30 +0000

    Jira #654: added suggested indexes from MPDL

--- src/migration/db/1.1.0/list.create.sql
+++ src/migration/db/1.1.0/list.create.sql
@@ -3,9 +3,6 @@ DROP INDEX list.property_path_value_position;
 
 ALTER TABLE list.property ALTER COLUMN value TYPE VARCHAR(2000);
 
-DROP INDEX list.property_id_path_value;
-DROP INDEX list.property_path_value_position;
-
 CREATE INDEX id_local_path_position
   ON list.property
   USING btree
--- src/migration/db/1.1.0/mysql/list.create.sql
+++ src/migration/db/1.1.0/mysql/list.create.sql
@@ -3,8 +3,22 @@ DROP INDEX property_path_value_position ON list.property;
 
 ALTER TABLE list.property CHANGE value value VARCHAR(300);
 
-CREATE INDEX property_id_path_value USING btree
-  ON list.property (resource_id(300), local_path(300), value);
+CREATE INDEX id_local_path_position
+  USING btree
+  ON list.property
+  (resource_id(300), local_path(300), position);
 
-CREATE INDEX property_path_value_position USING btree
-  ON list.property (local_path(300), value, position);
+CREATE INDEX id_local_path_value
+  USING btree
+  ON list.property
+  (resource_id(300), local_path(300), value);
+
+CREATE INDEX local_path_value_position
+  USING btree
+  ON list.property
+  (local_path(300), value, position);
+
+CREATE INDEX local_path_value_id
+  USING btree
+  ON list.property
+  (local_path(300), value, resource_id(300));
