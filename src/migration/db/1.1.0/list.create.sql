c106c11cb7893193d2e4484b4f1d77baadb53f0d
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2009-04-01 13:42:03 +0000

    Jira #654: added suggested indexes from MPDL

--- src/migration/db/1.1.0/list.create.sql
+++ src/migration/db/1.1.0/list.create.sql
@@ -3,8 +3,31 @@ DROP INDEX list.property_path_value_position;
 
 ALTER TABLE list.property ALTER COLUMN value TYPE VARCHAR(2000);
 
-CREATE INDEX property_id_path_value ON list.property
-  USING btree (resource_id, local_path, value);
+DROP INDEX list.property_id_path_value;
+DROP INDEX list.property_path_value_position;
 
-CREATE INDEX property_path_value_position ON list.property
-  USING btree (local_path, value, position);
+CREATE INDEX id_local_path_position
+  ON list.property
+  USING btree
+  (resource_id, local_path, "position");
+
+CREATE INDEX id_local_path_context_id
+  ON list.property
+  USING btree
+  (resource_id, local_path, value)
+  WHERE local_path = '/properties/context/id'::text;
+
+CREATE INDEX id_local_path_value
+  ON list.property
+  USING btree
+  (resource_id, local_path, value);
+
+CREATE INDEX local_path_value_position
+  ON list.property
+  USING btree
+  (local_path, value, "position");
+
+CREATE INDEX local_path_value_id
+  ON list.property
+  USING btree
+  (local_path, value, resource_id);
