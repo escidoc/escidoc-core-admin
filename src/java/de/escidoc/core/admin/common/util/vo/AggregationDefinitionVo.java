59d0beda7983b5b0669ecdc930c0675bc64d91ff
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2009-12-07 13:53:57 +0000

    vos for aggregation-definition

---
+++ src/java/de/escidoc/core/admin/common/util/vo/AggregationDefinitionVo.java
@@ -0,0 +1,84 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or http://www.escidoc.de/license.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2009 Fachinformationszentrum Karlsruhe Gesellschaft
+ * fuer wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Foerderung der Wissenschaft e.V.
+ * All rights reserved.  Use is subject to license terms.
+ */
+
+package de.escidoc.core.admin.common.util.vo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.escidoc.core.sm.business.persistence.hibernate.AggregationStatisticDataSelector;
+import de.escidoc.core.sm.business.persistence.hibernate.AggregationTable;
+
+public class AggregationDefinitionVo {
+
+    private String id = null;
+    private Collection<AggregationTable> aggregationTables = 
+                                new ArrayList<AggregationTable>();
+    private Collection<AggregationStatisticDataSelector> 
+                    aggregationStatisticDataSelectors = 
+                        new ArrayList<AggregationStatisticDataSelector>();
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    /**
+     * @return the aggregationTables
+     */
+    public Collection<AggregationTable> getAggregationTables() {
+        return aggregationTables;
+    }
+    /**
+     * @param aggregationTables the aggregationTables to set
+     */
+    public void setAggregationTables(Collection<AggregationTable> aggregationTables) {
+        this.aggregationTables = aggregationTables;
+    }
+    /**
+     * @return the aggregationStatisticDataSelectors
+     */
+    public Collection<AggregationStatisticDataSelector> getAggregationStatisticDataSelectors() {
+        return aggregationStatisticDataSelectors;
+    }
+    /**
+     * @param aggregationStatisticDataSelectors the aggregationStatisticDataSelectors to set
+     */
+    public void setAggregationStatisticDataSelectors(
+            Collection<AggregationStatisticDataSelector> aggregationStatisticDataSelectors) {
+        this.aggregationStatisticDataSelectors = aggregationStatisticDataSelectors;
+    }
+    
+}
---
+++ src/java/de/escidoc/core/admin/common/util/vo/AggregationTableVo.java
@@ -0,0 +1,107 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at license/ESCIDOC.LICENSE
+ * or http://www.escidoc.de/license.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at license/ESCIDOC.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright 2009 Fachinformationszentrum Karlsruhe Gesellschaft
+ * fuer wissenschaftlich-technische Information mbH and Max-Planck-
+ * Gesellschaft zur Foerderung der Wissenschaft e.V.
+ * All rights reserved.  Use is subject to license terms.
+ */
+
+package de.escidoc.core.admin.common.util.vo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class AggregationTableVo {
+    private String id = null;
+    private String name = null;
+    private int listIndex = 0;
+    private Collection<AggregationTableField> aggregationTableFields = 
+                                    new ArrayList<AggregationTableField>();
+    private Collection<AggregationTableIndexField> aggregationTableIndexFields = 
+                                    new ArrayList<AggregationTableIndexField>();
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * @return the listIndex
+     */
+    public int getListIndex() {
+        return listIndex;
+    }
+    /**
+     * @param listIndex the listIndex to set
+     */
+    public void setListIndex(int listIndex) {
+        this.listIndex = listIndex;
+    }
+    /**
+     * @return the aggregationTableFields
+     */
+    public Collection<AggregationTableField> getAggregationTableFields() {
+        return aggregationTableFields;
+    }
+    /**
+     * @param aggregationTableFields the aggregationTableFields to set
+     */
+    public void setAggregationTableFields(
+            Collection<AggregationTableField> aggregationTableFields) {
+        this.aggregationTableFields = aggregationTableFields;
+    }
+    /**
+     * @return the aggregationTableIndexFields
+     */
+    public Collection<AggregationTableIndexField> getAggregationTableIndexFields() {
+        return aggregationTableIndexFields;
+    }
+    /**
+     * @param aggregationTableIndexFields the aggregationTableIndexFields to set
+     */
+    public void setAggregationTableIndexFields(
+            Collection<AggregationTableIndexField> aggregationTableIndexFields) {
+        this.aggregationTableIndexFields = aggregationTableIndexFields;
+    }
+    
+
+}
