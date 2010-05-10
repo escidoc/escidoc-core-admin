48f43aab98047df6a8b1ac0b8f8b3fe8ee335d9a
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2010-05-10 11:08:32 +0000

    delete role workflow-manager

---
+++ src/migration/db/1.3.0/aa.drop.roles.workflow-manager.sql
@@ -0,0 +1,7 @@
+DELETE FROM list.filter WHERE role_id = 'escidoc:role-workflow-manager';
+
+DELETE FROM aa.role_grant WHERE role_id = 'escidoc:role-workflow-manager';
+
+DELETE FROM aa.scope_def WHERE role_id = 'escidoc:role-workflow-manager';
+
+DELETE FROM aa.escidoc_role WHERE id = 'escidoc:role-workflow-manager';
