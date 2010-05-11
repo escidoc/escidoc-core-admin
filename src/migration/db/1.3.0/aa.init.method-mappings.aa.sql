7081515a43f5c397fde0613413c0be4a4188e771
MichaelHoppe <Michael.Hoppe@fiz-karlsruhe.de>
2010-05-11 13:22:26 +0000

    correct order

--- src/migration/db/1.3.0/aa.init.method-mappings.aa.sql
+++ src/migration/db/1.3.0/aa.init.method-mappings.aa.sql
@@ -1,4 +1,4 @@
-DELETE FROM aa.method_mappings WHERE id='escidoc:mm-roles-retrieve';
-
 DELETE FROM aa.invocation_mappings WHERE id='escidoc-im-roles-retrieve-1';
+
+DELETE FROM aa.method_mappings WHERE id='escidoc:mm-roles-retrieve';
 
