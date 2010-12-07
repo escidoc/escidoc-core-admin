aabbfa8a52117a7cdcd331cefb840cbefad73ad0
sche <sche>
2010-12-07 17:14:04 +0000

    removed recaching

--- data/sql/list.clear.sql
+++
@@ -1,7 +0,0 @@
-TRUNCATE list.container;
-TRUNCATE list.content_model;
-TRUNCATE list.content_relation;
-TRUNCATE list.context;
-TRUNCATE list.item;
-TRUNCATE list.ou;
-TRUNCATE list.property;
--- data/sql/mysql.list.clear.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM list.container;
-DELETE FROM list.content_relation;
-DELETE FROM list.context;
-DELETE FROM list.item;
-DELETE FROM list.ou;
-DELETE FROM list.property;
